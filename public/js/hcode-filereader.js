1c4ae1109817777c8b23fd0f899aecdecbc4ade0
Vinicus <viniciusalcantarabarros@gmail.com>
2021-07-01 14:27:43 -0300

    Criando plugin para imagem

--- public/js/hcode-filereader.js
+++ public/js/hcode-filereader.js
@@ -6,26 +6,27 @@ class HcodeFileReader {
         this.initInputEvent()
     }
 
-    initInputEvent(){
-        console.log(document.querySelector(this.inputEl))
-        document.querySelector(this.inputEl).addEventListener("change", e=> {
+    initInputEvent(){        
+        document.querySelector(this.inputEl).addEventListener("change", e=> {                
             this.reader(e.target.files[0]).then(result => {
-                document.querySelector(this.inputEl).src = result
+                document.querySelector(this.imgEl).src = result
             })
         })
     }
 
     reader(file){
-        let reader = new FileReader();
+        return new Promise((resolve, reject) => {
+            let reader = new FileReader();
         
-        reader.onload = function(){
-            resolve(reader.result)
-        }
+            reader.onload = function(){            
+                resolve(reader.result)
+            }
 
-        reader.onerror = function(){
-            reject("Não foi possível ler a imagem")
-        }
+            reader.onerror = function(){
+                reject("Não foi possível ler a imagem")
+            }
 
-        reader.readAsDataURL(file)
+            reader.readAsDataURL(file)
+        })
     }
 }
