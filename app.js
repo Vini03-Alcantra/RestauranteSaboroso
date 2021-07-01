afa35bc154d2d6f9c0e1cc165f7c0f9425aeb24b
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-30 23:20:48 -0300

    Resolvendo bug de FileReader

--- app.js
+++ app.js
@@ -16,22 +16,28 @@ var adminRouter = require('./routes/admin');
 var app = express();
 
 app.use(function(req, res, next){
-    if (req.method === "POST") {
-      var form = formidable.IncomingForm({
-        uploadDir:path.join(__dirname, "/public/images"),
-        keepExtensions: true
-      });
-
-      form.parse(req, function(err, fields, files){
-        req.fields = fields;
-        req.files = files
-
-        next()
-      })
-    } else {
-      next()
-    }
-})
+ 
+  let contentType = req.headers["content-type"];
+ 
+  if (req.method === 'POST' && contentType.indexOf('multipart/form-data;') > -1) {
+    var form = formidable.IncomingForm({
+      uploadDir: path.join(__dirname, "/public/images"),
+      keepExtensions: true
+    });
+ 
+    form.parse(req, function(err, fields, files){
+ 
+      req.fields = fields;
+      req.files = files;
+ 
+      next();
+ 
+    });
+  } else {
+    next();
+  }
+ 
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
